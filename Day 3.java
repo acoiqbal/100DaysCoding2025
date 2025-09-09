bd997687b1f6a94d84f9932046984a9c398adb31
acoiqbal <acoikbalikbal@gmail.com>
2025-09-10 01:43:51 +0800

    Memahami dan menggunakan Komentar

---
+++ Day 3.java
@@ -0,0 +1,22 @@
+public class Main {
+  public static void main(String[] args) {
+     //System.out.println adalah pernyataan dalam bahasa pemrograman Java yang digunakan
+     //untuk mencetak output ke konsol.
+     System.out.println("Nama :Aco iqbal");
+
+     //System.out.print  adalah pernyataan dalam bahasa pemrograman Java yang digunakan
+     //untuk mencetak output ke konsol tanpa mengakhiri baris.
+       System.out.print("Nim :");
+       System.out.print("D0223041\n");
+              
+     /*System.out.printf  adalah pernyataan dalam bahasa pemrograman Java yang digunakan
+       untuk format pencetakan output ke konsol.
+       dapat menggunakan format specifier seperti  %d  untuk integer,  %f  untuk float,  %s  untuk string, 
+       dan sebagainya untuk mengatur tampilan output sesuai dengan format yang diinginkan.*/
+        int sc = 20;
+        char sj = 'e';
+            
+        System.out.printf("Umur: %d\n", sc);
+        System.out.printf("Kelas: %C",sj);
+       }
+}
