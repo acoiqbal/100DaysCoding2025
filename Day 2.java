09ebaee7e2350af3e04216aef20c22ce3b9f8195
acoiqbal <acoikbalikbal@gmail.com>
2025-09-09 08:19:13 +0800

    Penggunaan println(), print(), dan printf()

---
+++ Day 2.java
@@ -0,0 +1,14 @@
+public class Main {
+       public static void main(String[] args) {
+          System.out.println("Nama :Aco iqbal");
+              
+          System.out.print("Nim :");
+          System.out.print("D0223041\n");
+              
+          int sc = 20;
+          char sj = 'e';
+            
+           System.out.printf("Umur: %d\n", sc);
+           System.out.printf("Kelas: %C",sj);
+       }
+}
