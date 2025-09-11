53f70cc9ec3f4767bf9f6ecdffbbcd107ac63233
acoiqbal <acoikbalikbal@gmail.com>
2025-09-11 20:01:24 +0800

    Tipe Data Numerik Bilangan Bulat (byte, short, int, long).

---
+++ Day 5.java
@@ -0,0 +1,13 @@
+public class Day5 {
+    public static void main(String[] args) {
+        byte a = 100;
+        short b = 2004;
+        int c = 1000000;
+        long d = 1000000000000l;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
