55c888f418a60ecbfe0deb033d12a96f0cb683ad
acoiqbal <acoikbalikbal@gmail.com>
2025-09-11 11:18:36 +0800

    Penggunaan Escape Sequence (\n, \t dan lain-lain).

---
+++ Day 4.java
@@ -0,0 +1,13 @@
+public class Day4 {
+    public static void main(String[] args) {
+        System.out.println("No.\tNama\t\tNim");
+        System.out.println("-----------------------------------------");
+
+        System.out.println("1.\tAco iqbal\tD0223041");
+        System.out.println("2.\tDaud\t\tD0223047");
+        System.out.println("2.\tRajab\t\tD0223518");
+        System.out.println("2.\tDaud\t\tD0223507");
+
+        System.out.println("\nini adalah daftar nama mahasiswa \"Informatika\"");
+    }
+}
