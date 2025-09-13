106f97f5d5e8f16c8aa3ff0166eaade62002d577
acoiqbal <acoikbalikbal@gmail.com>
2025-09-13 08:08:17 +0800

    Tipe Data Pecahan (double, float).

---
+++ Day 6.java
@@ -0,0 +1,10 @@
+public class Day6 {
+    public static void main(String[] args) {
+        float ipk = 3.9f;
+        double tinggi = 171.5;
+
+        System.out.println("Nama = Aco iqbal");
+        System.out.println("IPK = " + ipk);
+        System.out.println("Tinggi badan = " + tinggi);
+    }
+}
