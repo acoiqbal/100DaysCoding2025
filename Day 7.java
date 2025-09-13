02b90ef0634ac524c267dccada2a460072600be8
acoiqbal <acoikbalikbal@gmail.com>
2025-09-13 17:28:17 +0800

    Tipe Data Karakter (char) dan Boolean (boolean).

---
+++ Day 7.java
@@ -0,0 +1,23 @@
+public class Day7 {
+    public static void main(String[] args) {
+        System.out.println("Nama  : Aco Iqbal");
+        char kelas = 'E';
+        System.out.println("Kelas : " + kelas);
+
+        System.out.println("-----------------------------------------");
+
+        boolean a = true;
+        boolean b = false;
+        System.out.println(a);
+        System.out.println(b);
+
+        System.out.println("-----------------------------------------");
+
+        boolean c = 5 > 10;
+        boolean d = 10 > 5;
+
+        System.out.println(c);
+        System.out.println(d);
+        
+    }
+}
