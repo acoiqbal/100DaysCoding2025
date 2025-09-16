4a3b5b04acecbc07aa0332db9e629bcb8e78be2d
acoiqbal <acoikbalikbal@gmail.com>
2025-09-16 15:08:09 +0800

    Deklarasi, Inisialisasi dan Update Variabel.

---
+++ Day 9.java
@@ -0,0 +1,18 @@
+public class Day9 {
+    public static void main(String[] args) {
+        //Deklarasi
+        String nama;
+        int umur;
+
+        //Inisialisasi
+        nama = "Aco iqbal";
+        umur = 20;
+        
+        //Update Variabel
+        umur = 21;
+
+        System.out.println("Nama : " + nama);
+        System.out.println("Umur : " + umur);
+        
+    }
+}
