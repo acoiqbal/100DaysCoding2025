a7b88b1e9807c87c308f5feaa38d381efebfeea0
acoiqbal <acoikbalikbal@gmail.com>
2025-09-22 23:37:39 +0800

    Operator Aritmatika Penjumlahan (+) dan Pengurangan (-)

---
+++ Day 15.java
@@ -0,0 +1,18 @@
+publid class Main{
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 10;
+
+    int hasil = a + b;
+
+    System.out.println("hasil penjumlahan a + b = " + hasil);
+
+    int c = 20;
+    int d = 10;
+
+    int hasil1 = c - d;
+
+    System.out.println("hasil pengurangan c - d = " + hasil1);
+
+  }
+}
