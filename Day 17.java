ab028f762c18ec40a8a6669693d002a996185b10
acoiqbal <acoikbalikbal@gmail.com>
2025-09-24 19:29:47 +0800

    Operator Aritmatika Modulus atau Sisa Bagi (%)

---
+++ Day 17.java
@@ -0,0 +1,12 @@
+public class Main {
+    
+    public static void main(String[] args) {
+    int a = 15;
+    int b = 2;
+
+    int hasil = a % b;
+
+    System.out.println("hasil sisa bagi a + b = " + hasil);
+
+    }
+}
