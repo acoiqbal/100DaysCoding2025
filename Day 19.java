b947dd907bdb2e233c73962de008948271a7731c
acoiqbal <acoikbalikbal@gmail.com>
2025-09-26 20:55:30 +0800

    Konversi Manual/Paksa (Dari Tipe Data Besar ke Kecil).

---
+++ Day 19.java
@@ -0,0 +1,15 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        int a = 1234;
+        byte b = 125;
+        
+        byte c = (byte) a;
+        double d = (double) b;
+            
+        System.out.println("A = " + c);
+        System.out.println("B = " + d);
+        
+        System.out.println("Sekian");
+    }
+}
