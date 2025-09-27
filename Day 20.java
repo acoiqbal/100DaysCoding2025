28deeac9103e352d4e2c478bf5e44b0c3c7c480a
acoiqbal <acoikbalikbal@gmail.com>
2025-09-27 17:00:03 +0800

    Mengubah Tipe Data Primitif menjadi String.

---
+++ Day 20.java
@@ -0,0 +1,14 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        int a = 123;
+        double b = 12.3;
+        
+        String c = String.valueOf(a);
+        String d = String.valueOf(b);
+        
+        System.out.println("A = " + c);
+        System.out.println("B = " + d);
+        System.out.println("Sekian");
+    }
+}
