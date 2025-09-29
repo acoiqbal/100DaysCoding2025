9595523e76bef243390f2d934da98011f2df7f86
acoiqbal <acoikbalikbal@gmail.com>
2025-09-29 20:08:48 +0800

    Latihan: Program Luas Persegi.

---
+++ Day 22.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int sisi = 8;
+        int luas;
+        
+        luas = sisi * sisi;
+        
+        System.out.println("Panjang sisi persegi adalah: " + sisi + " cm");
+        System.out.println("Luas persegi adalah: " + luas + " cm^2");
+    }
+}
