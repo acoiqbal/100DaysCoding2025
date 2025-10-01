4916b10101382878852560147d8d268e55198337
acoiqbal <acoikbalikbal@gmail.com>
2025-10-01 20:27:20 +0800

    Latihan: Program Luas Lingkaran.

---
+++ Day 24.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        final double PI = 3.14;
+        double jarijari = 10.0;
+        double luas;
+        
+        luas = PI * jarijari * jarijari;
+        
+        System.out.println("nilai PI: " + PI);
+        System.out.println("Jari-jari: " + jarijari + " cm");
+        System.out.println("Luas lingkaran adalah: " + luas + " cm^2");
+    }
+}
