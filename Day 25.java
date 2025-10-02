f1b60feba24658649a934d852c7cb8f7d9c10998
acoiqbal <acoikbalikbal@gmail.com>
2025-10-02 18:37:27 +0800

    Operator Increment dan Decrement (++, --)

---
+++ Day 25.java
@@ -0,0 +1,12 @@
+public class Day25 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("di increment = " + a++);
+        System.out.println("setelah di increment = " + a);
+        System.out.println("-----------------------");
+
+        int b = 11;
+        System.out.println("di decrement = " + b--);
+        System.out.println("setelah di decrement = " + b);
+    }
+}
