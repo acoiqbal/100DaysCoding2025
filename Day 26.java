54016b3408536f779c41b320d75ef671f8fe7295
acoiqbal <acoikbalikbal@gmail.com>
2025-10-03 15:58:13 +0800

    Operator Penugasan (=, +=, -=, dll).

---
+++ Day 26.java
@@ -0,0 +1,24 @@
+public class Day26 {
+    public static void main(String[] args) {
+        int a = 10;
+        a += 5;
+
+        int b = 10;
+        b -= 5;
+
+        int c = 10;
+        c /= 5;
+
+        int d = 10;
+        d *= 5;
+
+        int e = 15;
+        e %= 2;
+
+        System.out.println("tambah : " + a);
+        System.out.println("kurang : " + b);
+        System.out.println("pembagian : " + c);
+        System.out.println("perkalian : " + d);
+        System.out.println("sisa bagi : " + e);
+    }
+}
