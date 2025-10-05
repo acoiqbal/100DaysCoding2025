ff44c0a183892c7f0132d996822f8b5a275dd6ee
acoiqbal <acoikbalikbal@gmail.com>
2025-10-05 17:25:36 +0800

    Operator Perbandingan == dan !=

---
+++ Day 28.java
@@ -0,0 +1,13 @@
+public class Day28 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        int c = 5;
+
+        boolean d = a == c;
+        boolean e = a != c;
+
+        System.out.println("Apakah nilai a sama dengan c : " + d);
+        System.out.println("Apakah nilai a tidak sama dengan c : " + e);
+    }
+}
