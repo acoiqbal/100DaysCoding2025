2a8927f5ca25f644be517375e226c76e77d44923
acoiqbal <acoikbalikbal@gmail.com>
2025-10-06 20:49:01 +0800

    Operator Perbandingan < dan >.

---
+++ Day 29.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        int c = 5;
+
+        boolean d = a > c;
+        boolean e = a < c;
+
+        System.out.println("Apakah nilai a lebih besar dengan c : " + d);
+        System.out.println("Apakah nilai a lebih kecil dengan c : " + e);
+    }
+}
