a5da963752801d3afa819ec4989f82457364cdfa
acoiqbal <acoikbalikbal@gmail.com>
2025-10-09 13:47:00 +0800

    Operator Logika OR (||).

---
+++ Day 32.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean a = true || false;
+        boolean b = false || true;
+        boolean c = false || false;
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
