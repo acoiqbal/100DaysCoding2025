4521e373eb200ed3f3c3bcc281253ec4caa23b27
acoiqbal <acoikbalikbal@gmail.com>
2025-10-12 19:13:51 +0800

    Percabangan (if)

---
+++ Day 35.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int nilai = 90;
+        int nilaiMinimum = 70;
+
+        if (nilai >= nilaiMinimum) {
+            System.out.println("Selamat, Anda LULUS!");
+        } else {
+            System.out.println("Maaf, Anda Tidak LULUS");
+        }
+    }
+}
