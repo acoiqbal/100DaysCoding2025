e909e693e3842e25122424ee5026125c5bb9c2c9
acoiqbal <acoikbalikbal@gmail.com>
2025-10-14 17:33:39 +0800

    Latihan: Menentukan bilangan ganjil atau genap

---
+++ Day 37.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int angka = 7; 
+
+        System.out.println("Mengecek angka: " + angka);
+        
+        if (angka % 2 == 0) {
+            System.out.println("Hasil: Ini adalah bilangan GENAP.");
+        } else {
+            System.out.println("Hasil: Ini adalah bilangan GANJIL.");
+        }
+    }
+}
