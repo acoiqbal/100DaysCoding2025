68566652e39392d90299c99de1888461f0769f6e
acoiqbal <acoikbalikbal@gmail.com>
2025-10-26 17:15:42 +0800

    Operator Ternary

---
+++ Day 49.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        
+        String keterangan = (a % 2 == 0) ? "Genap" : "Ganjil";
+        
+        System.out.println(a + " adalah " + keterangan);
+    }
+}
