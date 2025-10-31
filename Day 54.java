093da54fe833dc90d55783fc8a1983de6dd38ba1
acoiqbal <acoikbalikbal@gmail.com>
2025-10-31 16:44:05 +0800

    Keyword continue

---
+++ Day 54.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println("Mencetak angka ganjil:");
+        
+        for (int i = 1; i <= 10; i++) {
+            
+            if (i % 2 == 0) {
+                continue; 
+            }
+            System.out.println(i);
+        }
+        
+        System.out.println("Loop selesai.");
+    }
+}
