c290021dd29879d095709ae1ff3bfc43bb01820d
acoiqbal <acoikbalikbal@gmail.com>
2025-11-02 16:53:59 +0800

    Perulangan do-while

---
+++ Day 56.java
@@ -0,0 +1,10 @@
+public class Day56 {
+    public static void main(String[] args) {
+        int a = 1;
+
+        do {
+            System.out.println("Angka ke-" + a);
+            a++;
+        } while (a <= 5);
+    }
+}
