5af8d3218ba29ebc3e1c22839e53184da7f132a1
acoiqbal <acoikbalikbal@gmail.com>
2025-11-12 20:28:03 +0800

    Latihan: Membuat Pola Vertikal

--- Day 66.java
+++ Day 66.java
@@ -1,12 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        int total = 1;
-
         for (int i = 1; i <= 5; i++) {
             System.out.println("*");
         }
-
-        
     }
 }
