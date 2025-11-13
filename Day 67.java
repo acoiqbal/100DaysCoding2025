3a3c1ccbbba9a70a86ade485f44bb3b76077c6bf
acoiqbal <acoikbalikbal@gmail.com>
2025-11-13 17:52:00 +0800

    Latihan: Membuat Pola Horizontal

---
+++ Day 67.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int total = 1;
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("*");
+        }
+
+        
+    }
+}
