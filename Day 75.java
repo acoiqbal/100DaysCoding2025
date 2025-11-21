db8b8b8a7d26619f9a8c9e0a8136c593329ecc04
acoiqbal <acoikbalikbal@gmail.com>
2025-11-21 17:08:50 +0800

    Method String Dasar (length(), toUpperCase(), toLowerCase()).

---
+++ Day 75.java
@@ -0,0 +1,11 @@
+public class Day75 {
+    public static void main(String[] args) {
+
+        String nama = "Aco Iqbal";
+
+        System.out.println("Kalimat asli      : " + nama);
+        System.out.println("Jumlah karakter   : " + nama.length());
+        System.out.println("Huruf besar       : " + nama.toUpperCase());
+        System.out.println("Huruf kecil       : " + nama.toLowerCase());
+    }
+}
