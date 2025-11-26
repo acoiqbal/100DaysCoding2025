fd8499fd1c5ea48a760e2a3a7be98a1827394dbd
acoiqbal <acoikbalikbal@gmail.com>
2025-11-26 16:54:57 +0800

    Array 1D

---
+++ Day 80.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] nilai = {90, 85, 78, 92, 88};
+
+        System.out.println("Array: ");
+
+        for(int i = 0; i < nilai.length; i++) {
+            System.out.println("Index " + i + " = " + nilai[i]);
+        }
+    }
+}
