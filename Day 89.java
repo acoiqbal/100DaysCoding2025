e6f158ac5cf353eff6711d68fbdfecf493bbc711
acoiqbal <acoikbalikbal@gmail.com>
2025-12-05 18:08:50 +0800

    Method Void

---
+++ Day 89.java
@@ -0,0 +1,12 @@
+public class Day89 {
+    public static void matapelajaran() {
+        System.out.println("==pelajaran==");
+        System.out.println("1. Cloud Computing");
+        System.out.println("2. Robotik");
+        System.out.println("3. Pemrograman Mobile");
+    }
+
+    public static void main(String[] args) {
+        matapelajaran();
+    }
+}
