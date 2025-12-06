91f79b8a11389ab0fa26f226133ad7c68244272c
acoiqbal <acoikbalikbal@gmail.com>
2025-12-06 18:34:37 +0800

    Method non-void

---
+++ Day 90.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static int tambah(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int hasil = tambah(10, 20); 
+        System.out.println("hasil : " + hasil);   
+    }
+}
