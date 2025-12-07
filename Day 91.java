968193a8de8c45ce4d9e0779c14ba844203d1479
acoiqbal <acoikbalikbal@gmail.com>
2025-12-07 17:50:57 +0800

    Parameter method

---
+++ Day 91.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static int kurang(int a, int b) {
+        return a - b;
+    }
+    public static void main(String[] args) {
+        int hasil = kurang(50, 25); 
+        System.out.println("hasil : " + hasil);   
+    }
+}
