3af7b3b9caed47417fd01737e70af7652478872e
acoiqbal <acoikbalikbal@gmail.com>
2025-12-08 16:43:22 +0800

    Keyword return

---
+++ Day 92.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static int kali(int a, int b) {
+        return a * b;
+    }
+    public static void main(String[] args) {
+        int hasil = kali(5,5); 
+        System.out.println("hasil : " + hasil);   
+    }
+}
