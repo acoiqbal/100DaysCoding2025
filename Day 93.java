e6d10799724e534104451eb1e2351776fa16cec7
acoiqbal <acoikbalikbal@gmail.com>
2025-12-09 12:40:59 +0800

    Method ceil(), floor() dan round()

---
+++ Day 93.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double angka = 7.3;
+
+        System.out.println("Angka asli : " + angka);
+        System.out.println("ceil()     : " + Math.ceil(angka));
+        System.out.println("floor()    : " + Math.floor(angka));
+        System.out.println("round()    : " + Math.round(angka));
+    }
+}
