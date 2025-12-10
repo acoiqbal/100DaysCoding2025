1907b7dc29c03295f1ed0c7ece071b4b9e29f77b
acoiqbal <acoikbalikbal@gmail.com>
2025-12-10 20:06:09 +0800

    Latihan: Mencetak dengan Method Void

---
+++ Day 94.java
@@ -0,0 +1,10 @@
+public class Main {
+    
+    public static void tampilPesan() {
+        System.out.println("Halo! Nama saya Aco iqbal.");
+    }
+
+    public static void main(String[] args) {
+        tampilPesan(); 
+    }
+}
